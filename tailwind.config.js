30d5d42dd571b51e53aeee5fb7aa3b7b65533b40
michael-pessoni <pessoni94@gmail.com>
2023-05-09 17:15:45 -0300

    Implement changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -7,5 +6,4 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
   ],
-}
-
+};
